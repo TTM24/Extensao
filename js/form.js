2c44a3c96115be92f74b8562cb7527197f084ca6
TTM24 <tiago.tomas.marquez@gmail.com>
2020-04-27 16:17:08 +0100

    Funcionar PDF e HTML no form a 100%

--- js/form.js
+++ js/form.js
@@ -3,68 +3,93 @@ let objeto = bgpage.word
 
 console.log(objeto);
 
-  //set valor DOI
-$(function () {
-  $('#doi').val(objeto.doi);
-});
+$('#selectArticle').change(function () {
+  if($('#selectArticle').val() == "Web Article") {
 
-//set valor titulo
-$(function () {
-  $('#title').val(objeto.titulo);
-});
-
-//set valor autor info
-$(function () {
-  $('#autor').val(objeto.autor);
-});
-
-//set valor ano
-$(function () {
-  $('#ano').val(objeto.ano);
-});
-
-//set valor info
-$(function () {
-  $('#info').val(objeto.info);
-});
-
-//set valor asbract
-$(function () {
-  $('#abstract').val(objeto.resumo);
-});
-
-//Lê avaliação
-$("input:radio[name='rating']").change(function () {
+    console.log("WebArticle");
+  
+    //set valor DOI
+    $(function () {
+      $('#doi').val(objeto.doi);
+    });
+    
+    //set valor titulo
+    $(function () {
+      $('#title').val(objeto.titulo);
+    });
+    
+    //set valor autor info
+    $(function () {
+      $('#autor').val(objeto.autor);
+    });
+    
+    //set valor ano
+    $(function () {
+      $('#ano').val(objeto.ano);
+    });
+    
+    //set valor info
+    $(function () {
+      $('#info').val(objeto.info);
+    });
+    
+    //set valor asbract
+    $(function () {
+      $('#abstract').val(objeto.resumo);
+    });
+  
+    //Lê avaliação
+  $("input:radio[name='rating']").change(function () {
     let rating = $('input[name="rating"]:checked').val();
     console.log(rating);
+  
+  });
+  
+  }
+
+  if($('#selectArticle').val() == "PDF"){
+
+    $('#reviewer').val("");
+    $('#doi').val("");
+    $('#title').val("");
+    $('#autor').val("");
+    $('#ano').val("");
+    $('#info').val("");
+    $('#abstract').val("");
+
+    console.log("PDF");
+    $.get("http://127.0.0.1:5000/", function(data, status){
+    let dataObj = jQuery.parseJSON(data);
+    console.log(dataObj);
+  
+    //set valor autor
+    $(function () {
+      $('#autor').val(dataObj.autor);
+    });
+  
+    //set valor titulo
+    $(function () {
+    $('#title').val(dataObj.title);
+    });
+  
+    //set valor info
+    $(function () {
+    $('#info').val(dataObj.info);
+    });
+  
+    })
+  
+    //Lê avaliação
+  $("input:radio[name='rating']").change(function () {
+    let rating = $('input[name="rating"]:checked').val();
+    console.log(rating);
+  
+  });
+  }
+
+
 
 });
-
-
-/*preencher campos PDF
-if (objeto.ano = undefined){
-  console.log(objeto);
-  $.get("http://127.0.0.1:5000/", function(data, status){
-  let dataObj = jQuery.parseJSON(data);
-  console.log(dataObj);
-
-  //set valor autor
-  $(function () {
-    $('#autor').val(dataObj.autor);
-  });
-
-  //set valor titulo
-  $(function () {
-  $('#title').val(dataObj.title);
-  });
-
-  //set valor info
-  $(function () {
-  $('#info').val(dataObj.info);
-  });
-
-  })
-};*/
 
 $('#submit-btn').click(function(){
 	
@@ -74,6 +99,8 @@ $('#submit-btn').click(function(){
   icon: "success",
   button: "Ok!",
 });
+
+
 
 });
 
